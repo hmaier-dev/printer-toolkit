b1e4bec001cbb92722139b141ad1907d647b19f7
Hendrik Maier <hmaier@ipb-halle.de>
2023-02-07 10:41:46 +0100

    template for configuring your printer installation

---
+++ printer-config.ps1
@@ -0,0 +1,11 @@
+<#
+Config Script
+Storing variables
+#>
+
+# hostname_printer == Get-PrinterPort.Name
+$hostname_printer = ""                    
+# shown to the user on apps 
+$shown_printer_name = ""                 
+# only visible after install, install manually at first
+$driver_name = ""    
